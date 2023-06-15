cce0056891525bb33f967429935fe44148057d76
M. Ilham Anggis Bangkit Pamungkas <83936039+M-ILHAM-197@users.noreply.github.com>
2023-06-15 16:22:18 +0700

    Update setup_uas.py

--- setup_uas.py
+++ setup_uas.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import pickle
-from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
